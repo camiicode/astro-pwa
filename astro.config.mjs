53a0bf05f0b822981154340654adda5123e521bf
camiicode <dftp93@gmail.com>
2025-03-26 13:24:49 -0500

    Add VITE-PWA and Serve preview correctly

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,37 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import VitePWA from "@vite-pwa/astro";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    VitePWA({
+      registerType: 'autoUpdate', // Se actualiza automáticamente cuando hay cambios
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,svg,webp}'],
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.destination === 'document',
+            handler: 'NetworkFirst', // Prioriza la red, pero usa caché si no hay conexión
+            options: {
+              cacheName: 'html-cache',
+            },
+          },
+          {
+            urlPattern: ({ request }) => request.destination === 'image',
+            handler: 'CacheFirst', // Guarda imágenes en caché para cargarlas rápido
+            options: {
+              cacheName: 'image-cache',
+            },
+          },
+          {
+            urlPattern: ({ request }) => request.destination === 'script' || request.destination === 'style',
+            handler: 'StaleWhileRevalidate', // Usa caché pero actualiza en segundo plano
+            options: {
+              cacheName: 'assets-cache',
+            },
+          },
+        ],
+      },
+      manifest: true
+    }),
+  ],
+});
